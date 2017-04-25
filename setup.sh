e32ffb7e395143f773885d0e89a9779ce2d2a36e
Sizhuo Zhang <szzhang@mit.edu>
2017-04-24 20:00:03 -0400

    publish coherence fpgautils

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright (c) 2017 Massachusetts Institute of Technology
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# This script takes an optional argument for toolchains built for specific
+# subsets of the RISC-V ISA.
+
+# Points to root folder of repository
+export RISCY_HOME=$PWD
+# Points to toolchain
+export RISCY_TOOLS=$RISCY_HOME/tools/RV64G
+# Adding to path and ld library path
+export PATH=$RISCY_TOOLS/bin:$PATH
+export LD_LIBRARY_PATH=$RISCY_TOOLS/lib:$LD_LIBRARY_PATH
